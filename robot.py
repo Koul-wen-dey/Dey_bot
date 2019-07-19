0a62b4ca76595b09e3ab4e8f5c734084529d872c
Koul-wen-dey <53071165+Koul-wen-dey@users.noreply.github.com>
2019-07-19 13:14:07 +0800

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,1 +1,17 @@
-hey
+import discord
+from discord.ext import commands
+
+Dey = commands.Bot(command_prefix = '/')
+
+@Dey.event
+async def on_ready():
+    print("-----BOT_Dey is online-----\n")
+
+@Dey.event
+async def on_member_join(member):
+    print(f'[member] has came.\n')
+        '''
+    channel = Dey.get_channel(366077676097634304)
+    await channel.send(f'[member] has came.\n'
+        '''
+Dey.run("NjAxNjI2NTk0Nzc1NDAwNDc4.XTFLsQ.hqHxQuPo5vbDhiykBxvgQiqYJVM")
