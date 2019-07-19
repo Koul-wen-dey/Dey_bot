e14bddb1061f91df78da1f384629da55571f581c
Koul-wen-dey <53071165+Koul-wen-dey@users.noreply.github.com>
2019-07-19 13:58:13 +0800

    discord robot

--- robot.py
+++ robot.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
+import json
 
+with open('setting.json','r',encoding = 'utf8') as jset:
+    jdata = json.load(jset)
 Dey = commands.Bot(command_prefix = '/')
 
 @Dey.event
@@ -9,9 +12,16 @@ async def on_ready():
 
 @Dey.event
 async def on_member_join(member):
-    print(f'[member] has came.\n')
-        '''
-    channel = Dey.get_channel(366077676097634304)
-    await channel.send(f'[member] has came.\n'
-        '''
-Dey.run("NjAxNjI2NTk0Nzc1NDAwNDc4.XTFLsQ.hqHxQuPo5vbDhiykBxvgQiqYJVM")
+    print(f'{member} has came.\n')   
+    #channel = Dey.get_channel(366077676097634304)
+    #await channel.send(member + 'has came.')
+@Dey.event
+async def on_member_remove(member):
+    print(f'{member} has gone.\n')   
+    #channel = Dey.get_channel(366077676097634304)
+    #await channel.send(member + 'has gone.')
+
+@Dey.command()
+async def ping(ctx):
+    await ctx.send(Dey.latency*1000)
+Dey.run(jdata['TOKEN'])
